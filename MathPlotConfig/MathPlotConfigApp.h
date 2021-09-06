606c9a0ccb8ec86ff187ed8e784f738125505cdd
Lionel <lionel.reynaud@free.fr>
2021-09-06 17:51:04 +0200

    CodeBlock project for MathPlotConfig

---
+++ MathPlotConfig/MathPlotConfigApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      MathPlotConfigApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Lionel ()
+ * Created:   2021-01-24
+ * Copyright: Lionel ()
+ * License:
+ **************************************************************/
+
+#ifndef MATHPLOTCONFIGAPP_H
+#define MATHPLOTCONFIGAPP_H
+
+#include <wx/app.h>
+
+class MathPlotConfigApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // MATHPLOTCONFIGAPP_H
