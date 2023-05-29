6f0994f38332092ec4c19dbd7b33a8f5fe9e7917
Lionel <lionel.reynaud@free.fr>
2023-05-29 11:02:29 +0200

    Update with new wxSmith

--- MathPlotConfig/MathPlotConfig.h
+++ MathPlotConfig/MathPlotConfig.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_E1D3326BF670B452
+#define HEADER_E1D3326BF670B452
+
 /***************************************************************
  * Name:      MathPlotConfig.h
  * Purpose:   Defines Application Frame
@@ -81,96 +84,6 @@ class MathPlotConfigDialog: public wxDialog
     //*)
 
     //(*Identifiers(MathPlotConfigDialog)
-    static const long ID_STATICTEXT11;
-    static const long ID_TEXTCTRL3;
-    static const long ID_BUTTON5;
-    static const long ID_CHECKBOX8;
-    static const long ID_STATICTEXT15;
-    static const long ID_TEXTCTRL4;
-    static const long ID_TEXTCTRL5;
-    static const long ID_TEXTCTRL6;
-    static const long ID_TEXTCTRL7;
-    static const long ID_BUTTON7;
-    static const long ID_CHECKBOX5;
-    static const long ID_STATICTEXT18;
-    static const long ID_CHOICE11;
-    static const long ID_CHECKBOX10;
-    static const long ID_CHECKBOX13;
-    static const long ID_CHECKBOX16;
-    static const long ID_STATICTEXT22;
-    static const long ID_BUTTON10;
-    static const long ID_STATICTEXT23;
-    static const long ID_CHOICE13;
-    static const long ID_PANEL17;
-    static const long ID_STATICTEXT16;
-    static const long ID_CHOICE10;
-    static const long ID_STATICTEXT24;
-    static const long ID_CHOICE14;
-    static const long ID_STATICTEXT25;
-    static const long ID_CHOICE15;
-    static const long ID_CHECKBOX9;
-    static const long ID_BUTTON12;
-    static const long ID_STATICTEXT20;
-    static const long ID_BUTTON9;
-    static const long ID_STATICTEXT21;
-    static const long ID_CHOICE12;
-    static const long ID_PANEL15;
-    static const long ID_CHOICE7;
-    static const long ID_STATICTEXT8;
-    static const long ID_TEXTCTRL2;
-    static const long ID_BUTTON8;
-    static const long ID_STATICTEXT12;
-    static const long ID_BUTTON6;
-    static const long ID_STATICTEXT13;
-    static const long ID_CHOICE8;
-    static const long ID_STATICTEXT14;
-    static const long ID_CHOICE9;
-    static const long ID_CHECKBOX11;
-    static const long ID_STATICTEXT27;
-    static const long ID_TEXTCTRL9;
-    static const long ID_STATICTEXT28;
-    static const long ID_TEXTCTRL10;
-    static const long ID_STATICTEXT17;
-    static const long ID_CHOICE6;
-    static const long ID_CHECKBOX4;
-    static const long ID_CHECKBOX7;
-    static const long ID_STATICTEXT29;
-    static const long ID_CHOICE16;
-    static const long ID_STATICTEXT26;
-    static const long ID_TEXTCTRL8;
-    static const long ID_CHECKBOX15;
-    static const long ID_PANEL1;
-    static const long ID_STATICTEXT1;
-    static const long ID_CHOICE1;
-    static const long ID_BUTTON11;
-    static const long ID_STATICTEXT2;
-    static const long ID_TEXTCTRL1;
-    static const long ID_STATICTEXT3;
-    static const long ID_BUTTON1;
-    static const long ID_STATICTEXT4;
-    static const long ID_CHOICE2;
-    static const long ID_STATICTEXT5;
-    static const long ID_CHOICE3;
-    static const long ID_CHECKBOX1;
-    static const long ID_CHECKBOX2;
-    static const long ID_CHECKBOX3;
-    static const long ID_CHECKBOX6;
-    static const long ID_CHECKBOX14;
-    static const long ID_STATICTEXT6;
-    static const long ID_BUTTON2;
-    static const long ID_STATICTEXT7;
-    static const long ID_CHOICE4;
-    static const long ID_STATICTEXT9;
-    static const long ID_CHOICE5;
-    static const long ID_STATICTEXT10;
-    static const long ID_SPINCTRL2;
-    static const long ID_STATICTEXT19;
-    static const long ID_SPINCTRL1;
-    static const long ID_CHECKBOX12;
-    static const long ID_PANEL4;
-    static const long ID_NOTEBOOK1;
-    static const long ID_BUTTON3;
-    static const long ID_BUTTON4;
     //*)
 
     //(*Declarations(MathPlotConfigDialog)
@@ -284,3 +197,5 @@ class MathPlotConfigDialog: public wxDialog
 } // MathPlot namespace
 
 #endif // MATHPLOTCONFIGMAIN_H
+#endif // header guard 
+
