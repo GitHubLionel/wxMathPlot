8fd08f9d8b76e983244d72290aa2fdfaf652cf87
GitHubLionel <73015867+GitHubLionel@users.noreply.github.com>
2026-09-12 06:58:22 +0000

    Deploying to gh-pages from @ GitHubLionel/wxMathPlot@0fb75a9798a9a4f6c34559611974999373cf1ddc 🚀

--- menudata.js
+++ menudata.js
@@ -95,7 +95,8 @@ var menudata={children:[
 {text:"v",url:"functions_vars.html#index_v"},
 {text:"x",url:"functions_vars.html#index_x"},
 {text:"y",url:"functions_vars.html#index_y"}]},
-{text:"Enumerations",url:"functions_enum.html"}]}]},
+{text:"Enumerations",url:"functions_enum.html"},
+{text:"Enumerator",url:"functions_eval.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
